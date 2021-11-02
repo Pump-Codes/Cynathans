18aff5307ba8da1736f26d83ef7676b75c522cab
Pump <oppumps781@gmail.com>
2021-11-02 12:04:33 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+
+console.log("Cynathans is the best multipurpose bot")
+console.log("Cynathans is the best multipurpose bot")
+console.log("Cynathans is the best multipurpose bot")
+console.log("Cynathans is the best multipurpose bot")
+console.log("Cynathans is the best multipurpose bot")
